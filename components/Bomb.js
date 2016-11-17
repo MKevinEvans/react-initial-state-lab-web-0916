807e84f43f16b5b22d8bf1afa7198507e64ee920
mkevinevans <mkevinevans@gmail.com>
2016-11-17 16:52:30 -0500

    Done.

---
+++ components/Bomb.js
@@ -0,0 +1,26 @@
+const React = require('react');
+
+class Bomb extends React.Component{
+	constructor(props){
+		super(props)
+		this.state = {secondsLeft: this.props.initialCount}
+			}
+		
+	
+	render(){
+		if(this.state.secondsLeft !== 0){
+			return(
+				<div>
+				{this.state.secondsLeft} seconds left before I go boom!
+				</div>
+				)}
+		else {
+			return(
+				<div>
+				Boom!
+				</div>
+				)
+		}
+	}
+}
+module.exports = Bomb;
